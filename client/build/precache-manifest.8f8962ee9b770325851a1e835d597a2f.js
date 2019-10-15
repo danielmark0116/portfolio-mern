f277f544edba59b4545c26bbb89d49c99dd61489
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-10-15 12:47:26 +0200

    Add build folder

---
+++ client/build/precache-manifest.8f8962ee9b770325851a1e835d597a2f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15081ab0fd8455f38fe7cd03d5e625a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9439fb7f42df1861fc5d",
+    "url": "/static/css/2.964f09f3.chunk.css"
+  },
+  {
+    "revision": "f984df710eb9c6dfc3a3",
+    "url": "/static/css/main.e4f98ff7.chunk.css"
+  },
+  {
+    "revision": "9439fb7f42df1861fc5d",
+    "url": "/static/js/2.fb492363.chunk.js"
+  },
+  {
+    "revision": "f984df710eb9c6dfc3a3",
+    "url": "/static/js/main.6e033fb7.chunk.js"
+  },
+  {
+    "revision": "7f45875b99a5ef02142c",
+    "url": "/static/js/runtime-main.c2763ce9.js"
+  },
+  {
+    "revision": "ec4a17dee28afe9aa966487735d74578",
+    "url": "/static/media/actionBtn.ec4a17de.svg"
+  },
+  {
+    "revision": "50e7a2c9c4670009c23390ffae7426cc",
+    "url": "/static/media/arrow.50e7a2c9.svg"
+  },
+  {
+    "revision": "df6e926bf356aaf6297cb02fda3b2857",
+    "url": "/static/media/github.df6e926b.svg"
+  },
+  {
+    "revision": "2563c0ab6f9007f0ac7dc7c705bf12a8",
+    "url": "/static/media/instagram.2563c0ab.svg"
+  },
+  {
+    "revision": "e67d558b068a812de81a0b0480ca79d4",
+    "url": "/static/media/linkedin.e67d558b.svg"
+  },
+  {
+    "revision": "3f82d40c0c3e230a4290c8bdd420f1e7",
+    "url": "/static/media/portfolioColoredLogo@2x.3f82d40c.png"
+  }
+]);
