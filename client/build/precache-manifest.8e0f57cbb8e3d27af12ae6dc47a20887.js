7f4362dae0b6ae125636e9cf296fc85d43c7be4d
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-10-31 14:09:13 +0100

    Add build for heroku

---
+++ client/build/precache-manifest.8e0f57cbb8e3d27af12ae6dc47a20887.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448f06ecfefd00b44047b1fc8223f214",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52b33cc8269a4b7926f9",
+    "url": "/static/css/2.964f09f3.chunk.css"
+  },
+  {
+    "revision": "720ae043a55f9bf8ae54",
+    "url": "/static/css/main.e4f98ff7.chunk.css"
+  },
+  {
+    "revision": "52b33cc8269a4b7926f9",
+    "url": "/static/js/2.e1cc7416.chunk.js"
+  },
+  {
+    "revision": "720ae043a55f9bf8ae54",
+    "url": "/static/js/main.871041b4.chunk.js"
+  },
+  {
+    "revision": "7f45875b99a5ef02142c",
+    "url": "/static/js/runtime-main.c2763ce9.js"
+  },
+  {
+    "revision": "ec4a17dee28afe9aa966487735d74578",
+    "url": "/static/media/actionBtn.ec4a17de.svg"
+  },
+  {
+    "revision": "50e7a2c9c4670009c23390ffae7426cc",
+    "url": "/static/media/arrow.50e7a2c9.svg"
+  },
+  {
+    "revision": "df6e926bf356aaf6297cb02fda3b2857",
+    "url": "/static/media/github.df6e926b.svg"
+  },
+  {
+    "revision": "2563c0ab6f9007f0ac7dc7c705bf12a8",
+    "url": "/static/media/instagram.2563c0ab.svg"
+  },
+  {
+    "revision": "e67d558b068a812de81a0b0480ca79d4",
+    "url": "/static/media/linkedin.e67d558b.svg"
+  },
+  {
+    "revision": "3f82d40c0c3e230a4290c8bdd420f1e7",
+    "url": "/static/media/portfolioColoredLogo@2x.3f82d40c.png"
+  }
+]);
